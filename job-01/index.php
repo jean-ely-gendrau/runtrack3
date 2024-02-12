96af7d7a3db76c7c2743f9bbf6f853e41c263305
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-12 11:33:44 +0100

    updade job-01

---
+++ job-01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello JS</title>
+  <script>
+    alert("Hello Javascript!")
+  </script>
+</head>
+
+<body>
+  <h1>Hello Js !</h1>
+</body>
+
+</html>
