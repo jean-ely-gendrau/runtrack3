5e75ae510e4c9eae00f261b9869c0ae8ec3e3f90
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-12 12:57:57 +0100

    update job-06

--- job-06/script.js
+++ job-06/script.js
@@ -4,7 +4,9 @@ function fizzbuzz() {
 
   for (let index = min; index < max; index++) {
     let result = index;
-    if (index % 3 === 1) {
+    if (index % 3 === 1 && index % 5 === 1) {
+      result = "FizzBuzz";
+    } else if (index % 3 === 1) {
       result = "Fizz";
     } else if (index % 5 === 1) {
       result = "Buzz";
