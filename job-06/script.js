8dfafc9418d2cc4a3074dc02d93a4e0df63a1657
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-12 19:03:28 +0100

    restauration du job-04,job-06,job-07, avec la commande git reset

--- job-06/script.js
+++ job-06/script.js
@@ -1,1 +1,15 @@
+function fizzbuzz() {
+  for (let index = min; index < max; index) {
+    let result = index;
 
+    if (index % 3 === 1 && index % 5 === 1) {
+      result = "FizzBuzz";
+    } else if (index % 5 === 1) {
+      result = "Buzz";
+    } else if (index % 3 === 1) {
+      result = "Fizz";
+    }
+  }
+  console.log(result);
+}
+fizzbuzz();
--- job-08/script.js
+++ job-08/script.js
@@ -1,16 +1,39 @@
 function sommeNombresPremiers(num1, num2) {
   let min = 2;
-  let action = true;
+  let action1 = true;
+  let action2 = true;
 
   for (let number = min; min < num1; number++) {
     if (number >= 2) {
       //test du quotien de la division
       for (quotien = 2; quotien < number; quotien++) {
         if (number % quotien == 0) {
-          action = false;
+          action1 = false;
         }
       }
     }
   }
+
+  for (let number = min; min < num1; number++) {
+    if (number >= 2) {
+      //test du quotien de la division
+      for (quotien = 2; quotien < number; quotien++) {
+        if (number % quotien == 0) {
+          action2 = false;
+        }
+      }
+    }
+  }
+
+  if (action1 && action2) {
+    console.log(num1 + num2);
+  } else if (!action1 && !action2) {
+    console.log(`${action1} et ${action2} ne sont pas des nombres 1er`);
+  } else if (!action1) {
+    console.log(`${action1} n'est pas un nombre 1er`);
+  } else if (!action2) {
+    console.log(`${action2} n'est pas un nombre 1er`);
+  }
 }
+
 sommeNombresPremiers(10, 20);
