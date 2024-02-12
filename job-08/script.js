bff4d74f71f6d52488c24a1a95579b95871a1b32
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-12 16:55:12 +0100

    update source code

--- job-08/script.js
+++ job-08/script.js
@@ -1,1 +1,16 @@
+function sommeNombresPremiers(num1, num2) {
+  let min = 2;
+  let action = true;
 
+  for (let number = min; min < num1; number++) {
+    if (number >= 2) {
+      //test du quotien de la division
+      for (quotien = 2; quotien < number; quotien++) {
+        if (number % quotien == 0) {
+          action = false;
+        }
+      }
+    }
+  }
+}
+sommeNombresPremiers(10, 20);
