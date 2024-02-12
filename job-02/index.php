5f34d63471a612c3804c40637b357ff346c51692
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-12 11:36:48 +0100

    update job-02 - add file script.js

--- job-02/index.php
+++ job-02/index.php
@@ -5,10 +5,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hello JS</title>
+
 </head>
 
 <body>
   <h1>Hello Js !</h1>
+
+  <!-- Add JS -->
+  <script src="./script.js"></script>
 </body>
 
 </html>
