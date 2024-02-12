af1e2f96325fc9e6da3722e4f6e65161512b6645
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-13 00:07:49 +0100

    update job-09

---
+++ job-09/script.js
@@ -0,0 +1,19 @@
+function tri(numbers, order) {
+  console.log(numbers, order);
+  numebersAsTri = numbers.map((number, index) => {
+    console.log(number, index);
+    var tempNimbers =
+      index + 1 <= numbers.length - 1
+        ? numbers.slice(index + 1)
+        : numbers.slice(0, 1);
+    console.log("tempNimbers", tempNimbers);
+    tempNimbers.forEach((numberNP1) => {
+      if (numberNP1 > number) {
+        return number;
+      }
+    });
+  });
+}
+
+number = [10, 8, 6, 5, 72, 25];
+number.sort(tri(number, "asc"));
