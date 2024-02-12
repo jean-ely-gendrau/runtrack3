c3bfd56314cc9ac9997ce32dee7bb3568d11ee14
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-12 11:57:08 +0100

    init job-05

--- job-04/script.js
+++ job-04/script.js
@@ -1,9 +1,1 @@
-function bisextile(annee) {
-  result =
-    (annee % 4 === 0 && annee % 100 > 0) || annee % 400 === 0
-      ? `${annee} est une année bissextil`
-      : `${annee} n'est une année bissextil`;
-  console.log(result);
-}
 
-bisextile("2001");
