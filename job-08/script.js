c6a2dc1dba64af37583816cced6aa039d2d8aca2
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-12 20:49:19 +0100

    update job-08

--- job-08/script.js
+++ job-08/script.js
@@ -1,39 +1,37 @@
 function sommeNombresPremiers(num1, num2) {
-  let min = 2;
-  let action1 = true;
-  let action2 = true;
+  if (num1 <= 1 || num2 <= 1) return false; //RETURN
+  if (num1 === 2 || num2 === 2)
+    //RETURN
+    return `Somme des nombres premier ${num1} et ${num2} : ${num1 + num2}`;
 
-  for (let number = min; min < num1; number++) {
-    if (number >= 2) {
-      //test du quotien de la division
-      for (quotien = 2; quotien < number; quotien++) {
-        if (number % quotien == 0) {
-          action1 = false;
-        }
-      }
-    }
+  /* 
+    Note de mathématique merci a : (https://www.pourlascience.fr/sd/mathematiques/nombres-premiers-des-jumeaux-des-cousins-et-des-nombres-sexy-16720.php#:~:text=Gr%C3%A2ce%20au%20crible%20ou%20tout,%2C%20193%2C%20197%20et%20199.)
+    Un nombre premier est forcément supérieur à 1.
+    Un nombre premier est un nombre supérieur,  ne pouvant pas être divisé par un nombre entier autre que lui-même ou 1.
+
+    Effectuons une boucle qui parcourt tous les nombres supérieurs à 1 et inférieurs à lui-même. (note ci-dessus)
+    En utilisant l'opérateur modulo, nous pouvons obtenir le reste de la division sur chacun de ces nombres,
+    si l'un d'entre eux est égal à zéro. Il est alors possible de diviser le nombre autrement que par lui-même ou 1..
+    Alors ce n'est pas un nombre premier.
+ */
+  for (let i = 2; i < num1; i++) {
+    if (num1 % i === 0) return false; //RETURN
   }
 
-  for (let number = min; min < num1; number++) {
-    if (number >= 2) {
-      //test du quotien de la division
-      for (quotien = 2; quotien < number; quotien++) {
-        if (number % quotien == 0) {
-          action2 = false;
-        }
-      }
-    }
+  for (let i = 2; i < num2; i++) {
+    if (num2 % i === 0) return false; //RETURN
   }
 
-  if (action1 && action2) {
-    console.log(num1 + num2);
-  } else if (!action1 && !action2) {
-    console.log(`${action1} et ${action2} ne sont pas des nombres 1er`);
-  } else if (!action1) {
-    console.log(`${action1} n'est pas un nombre 1er`);
-  } else if (!action2) {
-    console.log(`${action2} n'est pas un nombre 1er`);
-  }
+  // Si tout c'est bien passé alors on retourne le résultat
+  return `Somme des nombres premier ${num1} et ${num2} : ${num1 + num2}`; //RETURN
 }
-
-sommeNombresPremiers(10, 20);
+// Fonction sommeNombresPremiers(int, int);
+result = sommeNombresPremiers(5, 5);
+// Fonction Ternaire
+!result
+  ? //Si la fonction retourne fasle on affiche une erreur : console.error(message)
+    console.error(
+      "Ooppps un des nombres n'est pas premier, ou une erreur insoupçonnée est survenu"
+    )
+  : // Sinon on affiche le résultat : console.log(result)
+    console.log(result);
