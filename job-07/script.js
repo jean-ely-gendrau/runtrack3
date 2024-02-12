f675e00076357892dfbc74a73c5a81f03fc575db
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-12 13:00:02 +0100

    update job-07 add function jourtravaille()

--- job-07/script.js
+++ job-07/script.js
@@ -1,18 +1,1 @@
-function fizzbuzz() {
-  let min = 1;
-  let max = 151;
-
-  for (let index = min; index < max; index++) {
-    let result = index;
-    if (index % 3 === 1 && index % 5 === 1) {
-      result = "FizzBuzz";
-    } else if (index % 3 === 1) {
-      result = "Fizz";
-    } else if (index % 5 === 1) {
-      result = "Buzz";
-    }
-
-    console.log(result);
-  }
-}
-fizzbuzz();
+function jourtravaille() {}
