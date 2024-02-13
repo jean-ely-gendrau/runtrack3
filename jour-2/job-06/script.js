6e1d66f09e3db932fd33f11bda56fee26eb21060
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-13 22:23:38 +0100

    update source code job-06

--- jour-2/job-06/script.js
+++ jour-2/job-06/script.js
@@ -1,5 +1,10 @@
 const konamiHandler = (e) => {
+  /* initialisation du tableau qui contiendra
+     les dernières touches appuyées.
+  */
   let arrayBufferNonami = [];
+
+  // Le code secret des touches appuyées. Ne faite par attention au majuscule et minuscule, nous verrons cela après.
   let konamiSecret = [
     "up",
     "up",
@@ -12,14 +17,15 @@ const konamiHandler = (e) => {
     "A",
     "B",
   ];
+
   if (arrayBufferNonami.length === konamiSecret.length) {
     /* Ici le code si l'utilisateur trouve le secret Konami */
   } else {
-    /* Sinon on va appliqué quelques actions */
+    konamiSecret.push(e.key);
   }
 };
 /* 
-    Nous allons écouter l'élement Window 
+    Nous allons écouter l'élément Window
     si keypress (appuye sur une touche) alors (Event e) => fonction(Event e)
 */
 window.addEventListener("keypress", (e) => konamiHandler(e)); // Ecouteur
