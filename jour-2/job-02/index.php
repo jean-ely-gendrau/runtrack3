26b365f044faf94adb5291f198d8afef0e818612
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-13 16:40:09 +0100

    update job-02 add style display none

--- jour-2/job-02/index.php
+++ jour-2/job-02/index.php
@@ -5,6 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Js - Jours 2</title>
+  <style>
+    .displayNone {
+      display: none;
+    }
+  </style>
 </head>
 
 <body>
