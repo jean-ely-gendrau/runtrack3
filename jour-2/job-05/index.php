bd95755158bd397e381a759fecb3be31ef330481
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-13 19:19:28 +0100

    update job-05 add style.css

--- jour-2/job-05/index.php
+++ jour-2/job-05/index.php
@@ -4,11 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style.css">
   <title>Js - Jours 2 - Job-05</title>
 </head>
 
 <body>
-  <textarea id="keylogger"></textarea>
+  <footer></footer>
   <!-- ADD JS -->
   <script src="./script.js"></script>
 </body>
