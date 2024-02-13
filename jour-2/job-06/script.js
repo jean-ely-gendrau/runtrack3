e786f5c5e5c94e7d25d5e89e7027b555bcbb3611
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-13 22:03:16 +0100

    update job-06 and add  comments

--- jour-2/job-06/script.js
+++ jour-2/job-06/script.js
@@ -1,8 +1,23 @@
+const konamiHandler = (e) => {
+  let arrayBufferNonami = [];
+  let konamiSecret = [
+    "up",
+    "up",
+    "down",
+    "down",
+    "left",
+    "right",
+    "left",
+    "right",
+    "A",
+    "B",
+  ];
+  if (arrayBufferNonami.length === konamiSecret.length) {
+    /* Ici le code si l'utilisateur trouve le secret Konami */
+  }
+};
 /* 
     Nous allons écouter l'élement Window 
     si keypress (appuye sur une touche) alors (Event e) => fonction(Event e)
-  */
-window.addEventListener(
-  "keypress",
-  (e) => /* Ajouter la fonction ICI */ addFunction(e) // remplacer addFunction
-); // Ecouteur
+*/
+window.addEventListener("keypress", (e) => konamiHandler(e)); // Ecouteur
