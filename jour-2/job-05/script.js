ed68177f605769f98276938a1774a9a03fe3675c
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-13 21:01:56 +0100

    update job-05 Scrolling and add footer const - update background opacity on scroll

--- jour-2/job-05/script.js
+++ jour-2/job-05/script.js
@@ -3,14 +3,29 @@ const Scrolling = (e) => {
   Déstucturation de document.documentElement
   On récupère dirrectement toutes les proprètés de l'object
   à l'intérieur des acolades.
-  
+
   scrollTop    => valeur du scroll en cour à partir du haut du dociument
   clientHeight => Hauteur de la fenêtre client (navigateur)
   scrollHeight => Hauteur total du document.
 */
   const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
 
-  scrollValu = Math.floor((scrollTop / (scrollHeight - clientHeight)) * 100);
-  return console.log(scrollTop, clientHeight, scrollHeight, scrollValu);
+  scrollValu = Math.floor((scrollTop / (scrollHeight - clientHeight)) * 100); // Pourcentage de défilement à partir du haut.
+
+  const footer = (scrollValu) =>
+    (document.querySelector("footer.footer").style.opacity = scrollValu / 100);
+
+  footer(scrollValu);
+
+  /* DEBUG
+  return console.log(
+    scrollTop,
+    clientHeight,
+    scrollHeight,
+    scrollValu,
+    scrollValu / 100
+  );
+  */
 };
+
 document.addEventListener("scroll", (e) => Scrolling(e));
