5c96f3a95561c7213f703b4a7c485f9623c6cf37
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-14 00:36:50 +0100

    update and upgrade source code - it's work

--- jour-2/job-06/script.js
+++ jour-2/job-06/script.js
@@ -1,9 +1,13 @@
+/* initialisation du tableau qui contiendra
+   les dernières touches appuyées.
+
+   Lors des précedant test le tableau était écrasser à chaque chargement.
+   Changont la porter du tableau en dehos de la fonction et remplaçon let par var
+*/
+var arrayBufferNonami = [];
+
 const konamiHandler = (e) => {
   e.preventDefault();
-  /* initialisation du tableau qui contiendra
-     les dernières touches appuyées.
-  */
-  let arrayBufferNonami = [];
 
   // Le code secret des touches appuyées. Ne faite par attention au majuscule et minuscule, nous verrons cela après.
   let konamiSecret = [
@@ -19,12 +23,11 @@ const konamiHandler = (e) => {
     "B",
   ];
 
-  let lengthKonamiKey =
-    arrayBufferNonami.length > 0 ? arrayBufferNonami.length - 1 : 0; // Nombre valeur dans le tableau - 1
-  console.log(arrayBufferNonami);
+  let lengthKonamiKey = arrayBufferNonami.length; // Nombre valeur dans le tableau
+  console.log(lengthKonamiKey);
   // Si c'est le 1er élément du tableau.
   if (lengthKonamiKey === 0) {
-    console.log(e.key);
+    console.log("1", e.key);
     arrayBufferNonami.push(e.key); // Ajoute le résultat dans le tableau à la suite.
   }
   // Condition Suivante
@@ -33,57 +36,59 @@ const konamiHandler = (e) => {
      au nombre d'éléments de la clé secrete.
     
      konamiSecret.length -1 | 0 à x valeurs.
-
+     
      Si la touche appuyèe correspont on déclenche le changement de style.
-  */
+     */
   else if (lengthKonamiKey === konamiSecret.length - 1) {
     // On parcourt le tableau des dernières touches appuyées.
     arrayBufferNonami.forEach((element, index) => {
       /*
-      On s'assure que le résultat est identique selon le positionnement de l'index.
-      Et aussi que la touche dernièrement capturée par l'écouteur d'événement
-      est identique à l'index final de la clé secret Konami | ainsi si la dernière valeur ne correspont pas on arrête la boucle
+        On verifie si une touche enregistré est différente dans le tableau en comparent
+        les index de la clé secret Konami && on vérifie si la dernière touche appuyées 
+        est différente de la dernière valeur de la clé.
+
+        Si c'est le  cas on remet à zéro les touches dernièrements enregistrées
+        et on arrête laboucle.
     */
       if (
-        element === konamiSecret[index] &&
-        e.key.toLowerCase() ===
+        element !== konamiSecret[index] &&
+        e.key.toLowerCase() !==
           konamiSecret[konamiSecret.length - 1].toLowerCase()
       ) {
-        alert("Konami");
-      }
-      // Sinon on remet à zéro les touches dernièrements enregistrées
-      else {
         arrayBufferNonami = [];
+        return;
       }
     });
+    alert("Konami"); // Resultat
   }
   //Sinon
   else {
-    // On parcourt le tableau des dernières touches appuyées.
-    arrayBufferNonami.forEach((element, index) => {
-      /*
-          On s'assure que le résultat est identique selon le positionnement de l'index.
-          Et aussi que la touche dernièrement capturée par l'écouteur d'événement
-          est identique à l'index en cours de la clé secret Konami | ainsi si la dernière valeur ne correspont pas on arrête la boucle
-        */
-      if (
-        element === konamiSecret[index] &&
-        e.key.toLowerCase() ===
-          konamiSecret[konamiSecret.length - 1 - lengthKonamiKey].toLowerCase()
-      ) {
-        console.log(e.key);
-        arrayBufferNonami.push(e.key); // Ajoute le résultat dans le tableau à la suite.
-      }
-      // Sinon on remet à zéro les touches dernièrements enregistrées
-      else {
-        arrayBufferNonami = [];
-      }
-    });
+    /* DEBUG
+      console.log(
+        e.key.toLowerCase(),
+        konamiSecret[keyLength].toLowerCase(),
+        keyLength
+    );
+    */
+
+    /*
+      On s'assure que la touche appuyées est identique à l'index en cours de la clé secret Konami
+    */
+    if (e.key.toLowerCase() === konamiSecret[lengthKonamiKey].toLowerCase()) {
+      arrayBufferNonami.push(e.key); // Ajoute le résultat dans le tableau à la suite.
+      return;
+    }
+    // Sinon on remet à zéro les touches dernièrements enregistrées
+    else {
+      arrayBufferNonami = [];
+    }
   }
+  console.log(arrayBufferNonami);
+  console.trace();
 };
 /* 
     /!\ Deprecated keypress
     Nous allons écouter l'élément Window
     si keydown (appuye sur une touche) alors (Event e) => fonction(Event e)
-*/
+    */
 window.addEventListener("keydown", (e) => konamiHandler(e)); // Ecouteur
