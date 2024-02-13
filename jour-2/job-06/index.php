9e994b31cfc4c2a1d90a125990a04018572925bf
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-13 21:33:18 +0100

    init commit job-06

---
+++ jour-2/job-06/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Js - Jours 2 - Job-06</title>
+</head>
+
+<body>
+  <h1>Code Konami</h1>
+  <p>J'ai inséré un célèbre code Konami dans notre script, si vous le trouvé alors une surprise vous attend.</p>
+  <p>Je vous donne un petit indice tout de même, si je vous dit Haut Haut, Bas ..., je ne vous en dit pas plus vous allez trouvez ! </p>
+  <!-- ADD JS -->
+  <script src="./script.js"></script>
+</body>
+
+</html>
---
+++ jour-2/job-06/script.js
@@ -0,0 +1,8 @@
+/* 
+    Nous allons écouter l'élement Window 
+    si keypress (appuye sur une touche) alors (Event e) => fonction(Event e)
+  */
+window.addEventListener(
+  "keypress",
+  (e) => /* Ajouter la fonction ICI */ addFunction(e) // remplacer addFunction
+); // Ecouteur
