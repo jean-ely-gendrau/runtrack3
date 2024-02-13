ddcd66de31fdda8fb3a643feaa32795661ffe682
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-13 19:18:15 +0100

    init job-05

---
+++ jour-2/job-05/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Js - Jours 2 - Job-05</title>
+</head>
+
+<body>
+  <textarea id="keylogger"></textarea>
+  <!-- ADD JS -->
+  <script src="./script.js"></script>
+</body>
+
+</html>
