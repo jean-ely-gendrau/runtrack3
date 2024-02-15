a874c0179a620a0c0c77c53c0071957773033828
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-15 18:18:25 +0100

    update source code jour-3 job-2 working progress

--- jour-3/job-02/index.php
+++ jour-3/job-02/index.php
@@ -11,7 +11,9 @@
 <body>
   <h1>Jour 3 Jquery - Job-02</h1>
 
-  <button id="addButton" onclick="randomImage()">Mélanger les images</button>
+  <button id="addButton">Démarrer la partie</button>
+
+  <p>Puzzle Arc en Ciel, regardez bien l'image, un fois que vous démarre la partie les pièces seront mélanger et vous devrai la reproduire à l'identique dans la fenêtre.</p>
 
   <div class="draggable-box ui-widget-content">
     <img id="draggable-1" src="./images/arc1.png">
@@ -22,9 +24,14 @@
     <img id="draggable-6" src="./images/arc6.png">
   </div>
 
-  <div id="drop" class="ui-widget-content">
-    <p>Dépose moi ICI</p>
+  <div class="content-drop wrapper">
+    <div class="drop one ui-widget-content">&nbsp;</div>
+    <div class="drop two ui-widget-content">&nbsp;</div>
+    <div class="drop three ui-widget-content">&nbsp;</div>
+    <div class="drop four ui-widget-content">&nbsp;</div>
+    <div class="drop five ui-widget-content">&nbsp;</div>
   </div>
+
   <!-- ADD SCRIPT -->
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js" integrity="sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=" crossorigin="anonymous"></script>
--- jour-3/job-02/script.js
+++ jour-3/job-02/script.js
@@ -1,34 +1,35 @@
 function randomImage() {
+  // Sélectionne les éléments dans le conteneurs
   elementsImg = $(".draggable-box").find("*");
+  elementsImg.remove(); // Suppréssion des élement du DOM
 
-  elementsImg.remove();
-
+  // array des clé des éléments précédament sélectionné - tout en filtrant les résultats et exclures certaine clé de celui-çi
   keyImage = Object.keys(elementsImg).filter(
     (item) => item != "length" && item != "prevObject"
   );
-  console.log(keyImage);
-  let elementRandom = [];
-  while (0 < keyImage.length) {
+
+  // On parcourt le tableau avec la function each Jquery
+  $(keyImage).each(function () {
+    // On définit un nombre aléatoire à partir de notre tableau keyImage
     indexRandom = Math.floor(Math.random() * keyImage.length);
-    console.log(elementsImg[keyImage[indexRandom]]);
-    elementRandom = $(elementsImg[keyImage[indexRandom]]);
+
+    // On ajoute l'élément dans le DOM
+    $(".draggable-box").append($(elementsImg[keyImage[indexRandom]]));
+
+    // On retire la valeur du tableau keyImage
     keyImage.splice(indexRandom, 1);
-    $(".draggable-box").append(elementRandom);
-  }
+  });
 }
 
 $(document).ready(function () {
   // Event Jquery on clicl
   $("#addButton").on("click", function () {
-    let paragraph = $("<p></p>").text(
-      "Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie."
-    ); // Création d'élément Jquery avec text
-    $("body").append(paragraph); // Ajout du text dans le body
+    randomImage();
   });
 
   $(".draggable-box").find("*").draggable({ revert: "invalid" });
 
-  $("#drop").on("dropover", function (event, ui) {
+  $(".drop").on("dropover", function (event, ui) {
     console.log(event, ui);
   });
 
@@ -36,9 +37,9 @@ $(document).ready(function () {
     console.log(event, ui);
   });
 
-  $("#drop").droppable({
-    drop: function () {
-      alert("dropped");
+  $(".drop").droppable({
+    drop: function (event, ui) {
+      console.log(event, ui);
     },
   });
 });
