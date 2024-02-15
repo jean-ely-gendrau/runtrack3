dc3fb53e88c315e2081f6c5433c81780f9da5d2d
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-15 10:31:47 +0100

    update jour-1 job-09 - test function Array.prototype.mySort(array, options)

--- jour-1/job-09/script.js
+++ jour-1/job-09/script.js
@@ -1,15 +1,57 @@
+Array.prototype.mySort = function (array, options) {
+  array.forEach((items, indexs) => {
+    array.forEach((item, index) => {
+      if (options === "asc" && array[index + 1] && item > array[index + 1]) {
+        array.splice(index, 1);
+
+        array.splice(index + 1, 0, item);
+      }
+
+      if (options === "desc" && array[index + 1] && item < array[index + 1]) {
+        array.splice(index, 1);
+
+        array.splice(index + 1, 0, item);
+      }
+      /*
+    if (array[index + 1] && item < array[index + 1]) {
+      array.splice(index, 1);
+
+      array.splice(index - 1, 0, item);
+    }
+    */
+    });
+  });
+  return console.log(this);
+};
+
+Array.prototype.mySort1 = function (value, hhh) {
+  return console.log(value, hhh, this);
+};
 function tri(numbers, order) {
+  return console.log(numbers);
+
+  /*
   console.log(numbers);
 
   const numberAsc = (a, b) => a - b;
-  const mapReturn = numbers.map((number, index) => {
-    numbers.forEach((element) => {
-      if (element < number) {
-        return element;
-      }
-    });
+  const mapReturn = numbers.map((number, index, array) => {
+    console.log(array);
+    if (numbers[index + 1] < number) {
+      return numbers[index + 1];
+    } else {
+      return number;
+    }
   });
+*/
+  /*
+  const mapReturn = numbers.map((number, index) => {
+    numbers.filter((numberFilter, index) => number - numberFilter);
+  });
+
+  
+  */
+  // return mapReturn;
 }
 
 number = [10, 8, 6, 5, 72, 25];
-console.log(number.sort(tri(number, "asc")));
+number.mySort(number, "desc");
