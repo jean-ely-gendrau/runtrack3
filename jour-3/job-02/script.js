1fe3c2ce40468a43e6dbf4cf8b6f2cc64788a5d4
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-15 13:22:58 +0100

    update job-02 test draggable and droppable api jquery

---
+++ jour-3/job-02/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  // Event Jquery on clicl
+  $("#addButton").on("click", function () {
+    let paragraph = $("<p></p>").text(
+      "Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie."
+    ); // Création d'élément Jquery avec text
+    $("body").append(paragraph); // Ajout du text dans le body
+  });
+
+  $("#draggable").draggable({ revert: "invalid" });
+
+  $("#drop").on("dropover", function (event, ui) {
+    console.log(event, ui);
+  });
+
+  $("drop").on("dropdeactivate", function (event, ui) {
+    console.log(event, ui);
+  });
+
+  $("#drop").droppable({
+    drop: function () {
+      alert("dropped");
+    },
+  });
+});
