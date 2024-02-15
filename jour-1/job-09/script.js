f70b28af2bde871b89fc1071bd244147b80a37ef
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-15 10:52:05 +0100

    update comments

--- jour-1/job-09/script.js
+++ jour-1/job-09/script.js
@@ -1,57 +1,37 @@
 Array.prototype.mySort = function (array, options) {
+  // On parcourt  le tableau, et pour chaque valeur on fera une boucle, pour le trier entièrement
   array.forEach((items, indexs) => {
+    // Pour chaque item du tableau on vérifie tous les éléments du tableau
     array.forEach((item, index) => {
+      // Ordre Croissant : Si l'élement en cours d'évaluation de la bloucle est supérieur au prochain du tableau
       if (options === "asc" && array[index + 1] && item > array[index + 1]) {
+        // On l'extrait du tableau
         array.splice(index, 1);
 
+        // On change sa position dans le de n + 1
         array.splice(index + 1, 0, item);
       }
 
+      //Ordre décroisant : Si l'Élément en cours d'évaluation de la boucle est inférieur au prochain du tableau
       if (options === "desc" && array[index + 1] && item < array[index + 1]) {
+        // On l'extrait du tableau
         array.splice(index, 1);
 
+        // On change sa position dans le de n + 1
         array.splice(index + 1, 0, item);
       }
-      /*
-    if (array[index + 1] && item < array[index + 1]) {
-      array.splice(index, 1);
-
-      array.splice(index - 1, 0, item);
-    }
-    */
     });
   });
   return console.log(this);
 };
 
-Array.prototype.mySort1 = function (value, hhh) {
-  return console.log(value, hhh, this);
-};
-function tri(numbers, order) {
-  return console.log(numbers);
+number = [10, 8, 6, 5, 72, 25]; // Tableau de nombre
 
-  /*
-  console.log(numbers);
-
-  const numberAsc = (a, b) => a - b;
-  const mapReturn = numbers.map((number, index, array) => {
-    console.log(array);
-    if (numbers[index + 1] < number) {
-      return numbers[index + 1];
-    } else {
-      return number;
-    }
-  });
+/* mySort
+  Fonction Array.prototype. NomDeLaFunction 
+  Imitation de la function Array.prototype.sort()
+  Cette fonction reçoit en paramètre un tableau et une valeur string (options) asc | des
+  asc  : pour trier les nombres croissant
+  desc : pour trier les nombres décroissants
 */
-  /*
-  const mapReturn = numbers.map((number, index) => {
-    numbers.filter((numberFilter, index) => number - numberFilter);
-  });
-
-  
-  */
-  // return mapReturn;
-}
-
-number = [10, 8, 6, 5, 72, 25];
 number.mySort(number, "desc");
