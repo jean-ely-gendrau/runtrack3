be80a49690653335896518873bbefbe24c9f8c29
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-16 13:09:03 +0100

    init jour 4 - Job-01 - fetch

---
+++ jour-4/job-01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jour 4 - Job 01 - Fecth</title>
+</head>
+
+<body>
+  <h1>Jour 4 - Job 01 - Fecth</h1>
+  <button id="button"></button>
+  <!-- ADD SCRIPT -->
+  <srcipt src="./script.js"></srcipt>
+</body>
+
+</html>
