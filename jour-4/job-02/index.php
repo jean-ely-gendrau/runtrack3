4a1e9266f277da854865b3b8a6cd4a8186c93078
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-16 14:27:51 +0100

    init - job-02

---
+++ jour-4/job-02/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jour 4 - Job 01 - Fecth</title>
+</head>
+
+<body>
+  <h1>Jour 4 - Job 02 - Fecth</h1>
+  <p>Exemple de tableau
+    <br /><br />
+    {
+    name: "Toulon",
+    address: "Av. Franklin Roosevelt",
+    city: "Toulon",
+    codePos: "83000",
+    creation:"2023"
+    }
+    <br /><br />
+    Indiquez simplement le nom de la clé pour voir le résultat
+  </p>
+  <input type="text" placeholder="la clé de l'élément">
+  <button id="button">Afficher</button>
+  <!-- ADD SCRIPT -->
+  <script src="./script.js"></script>
+</body>
+
+</html>
