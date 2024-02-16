800d2065c5509f8baa5e12c4dce50659fb1ebbd6
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-16 14:45:43 +0100

    init job-03 and add file default

---
+++ jour-4/job-03/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jour 4 - Job 01 - Fecth</title>
+</head>
+
+<body>
+  <h1>Jour 4 - Job 03 - Fecth</h1>
+
+
+  <!-- ADD SCRIPT -->
+  <script src="./script.js"></script>
+</body>
+
+</html>
