6b28164d6623aebc1b3b305557233ef7cbf77ea2
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-20 13:47:30 +0100

    update source code and comments - work success

--- jour-6/job-04/script.js
+++ jour-6/job-04/script.js
@@ -7,34 +7,51 @@ onresize = (event) => {
   const buttonBurger = document.createElement("button"); // Création du bputton buger
   const domElementBurger = document.getElementById("buttonBurger"); // Sélecteur du boutton burger
 
+  // Attribution de style pour le boutton
   buttonBurger.setAttribute(
     "style",
-    "width:55px; height:55px; background-color: red; position: fixed; right:0; top:0;"
+    "width:55px; height:55px; background-color: blue; color: #fff; position: fixed; right:0; top:0;"
   );
+  buttonBurger.textContent = "bb";
+  // Attribution d'un id pour le sélectionner
   buttonBurger.setAttribute("id", "buttonBurger");
 
-  const switchDisplayLink = (e) => {
+  // Fonction flécher pour faire disparître ou apparaître les liens dans le headers
+  const switchDisplayLink = (hide) => {
+    // Si il y à des éléments
     if (cssElement.length > 0) {
+      // Pour chaque élement
       for (let i = 0; i < cssElement.length; i++) {
-        cssElement[i].style.display == "inline-block"
-          ? cssElement[i].setAttribute("style", "display:none")
-          : cssElement[i].setAttribute("style", "display:inline-block");
+        if (hide) {
+          cssElement[i].setAttribute("style", "display:none"); // Alors on fait disparaître display:none
+        } else if (hide === false) {
+          cssElement[i].setAttribute("style", "display:inline-block");
+        } else {
+          // Si .style.display == "inline-block"
+          cssElement[i].style.display === "inline-block"
+            ? cssElement[i].setAttribute("style", "display:none") // Alors on fait disparaître display:none
+            : cssElement[i].setAttribute("style", "display:inline-block"); // Sinon display:inline-block
+        }
       }
     }
   };
 
+  // Si la taille de la fenêtre est supérieur ou égal à 768
   if (window.screen.width >= 768) {
     if (domElementBurger) {
-      switchDisplayLink();
-      domElementBurger.remove();
-    }
-  } else if (window.screen.width <= 767) {
-    if (!domElementBurger) {
-      switchDisplayLink();
-      headerSelector.append(buttonBurger);
+      switchDisplayLink(false); // Appel de la fonction
+      domElementBurger.remove(); // On retrire le button burger de l'affichage
     }
   }
-
+  // Si la valeur de la largeur de la fenêtre est inférieur ou égale à 768
+  else if (window.screen.width <= 767) {
+    // Si le button burger n'est pas dans le DOM
+    if (!domElementBurger) {
+      switchDisplayLink(true); // Appel de la fonction
+      headerSelector.append(buttonBurger); // On ajoute le boutton burger dans le DOM
+    }
+  }
+  // Si le boutton burger est dans le DOM
   if (domElementBurger)
-    domElementBurger.addEventListener("click", () => switchDisplayLink());
+    domElementBurger.addEventListener("click", () => switchDisplayLink(null)); // écouteur d'évenement 'click' alors on appel la fonction.
 };
