d75173aae3a23506898215844962588e9b755dda
jean-ely.gendrau <jean-ely.gendrau@laplateforme.io>
2024-02-25 22:16:32 +0100

    init jour-7 tailwind css

---
+++ jour-7/job-01/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="../src/output.css" rel="stylesheet">
+  <title>Jour-7 Tailwind CSS</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <a href="./index.php" title="Accueil">Accueil</a>
+      <a href="./index.php" title="Accueil">Connexion</a>
+      <a href="./index.php" title="Accueil">Inscriptio</a>
+      <a href="./index.php" title="Accueil">Recherche</a>
+    </nav>
+  </header>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <footer>
+    <ul>
+      <li><a href="./index.php" title="Accueil">Accueil</a></li>
+      <li><a href="./index.php" title="Accueil">Connexion</a></li>
+      <li><a href="./index.php" title="Accueil">Inscriptio</a></li>
+      <li><a href="./index.php" title="Accueil">Recherche</a></li>
+    </ul>
+  </footer>
+</body>
+
+</html>
---
+++ jour-7/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./job-01/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
